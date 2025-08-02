a1f8d695818b9e97e38f8fc3f4283557772bbdf9
Tikka.lk <111751043+tikkalk@users.noreply.github.com>
2025-08-02 14:13:24 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
   email: "theekshanaudara0@gmail.com",
-  password: "Amaya@0623",
+  password: "2004Amaya@0623",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,3 +41,4 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
