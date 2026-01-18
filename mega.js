5c7983605baf659d659d7957e6b93fcb81379ff6
Tikka.lk <111751043+tikkalk@users.noreply.github.com>
2026-01-18 20:58:36 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -4,8 +4,8 @@ import fs from "fs";
 
 // Mega authentication credentials
 const auth = {
-    email: "", // your mega account login email
-    password: "", // your mega account login password
+    email: "theekshanaudara0@gmail.com", // your mega account login email
+    password: "2004theeksh@1", // your mega account login password
     userAgent:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -81,3 +81,4 @@ export const download = (url) => {
         }
     });
 };
+
